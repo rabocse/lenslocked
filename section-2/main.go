d051c9bf9a939907a67dbfe90c67245c25a355b7
Alex <alexdescobars@gmail.com>
2023-03-06 12:06:08 +0000

    Basic custom router

--- section-2/main.go
+++ section-2/main.go
@@ -15,11 +15,33 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>  Contact Page </h1><p> To get in touch, email me at <a href=\"mailto:example@example.test\"> exampke@example.test </a>.")
 }
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+
+	case "/":
+
+		homeHandler(w, r)
+
+	case "/contact":
+
+		contactHandler(w, r)
+
+	default:
+
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Page Not Found")
+	}
+
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+
+	var router Router
 
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 
 }
