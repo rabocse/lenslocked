a915dbe75906db11679527a633399145b867c7e8
Alex <alexdescobars@gmail.com>
2023-03-06 10:39:25 +0000

    Adding section-2 dir

---
+++ section-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/rabocse/lenslocked
+
+go 1.19
---
+++ section-2/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my site!!!! </h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>  Contact Page </h1><p> To get in touch, email me at <a href=\"mailto:example@example.test\"> exampke@example.test </a>.")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
+
+}
