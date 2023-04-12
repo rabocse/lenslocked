0263f9e93e84b60650c36e24513d406e6d45f98f
Alex <alexdescobars@gmail.com>
2023-04-12 16:44:08 +0100

    Added styling..Although there is an error compared to the course

---
+++ section-8/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/rabocse/lenslocked/controllers"
+	"github.com/rabocse/lenslocked/templates"
+	"github.com/rabocse/lenslocked/views"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS,
+		"home.gohtml",
+		"tailwind.gohtml"))))
+
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS,
+		"contact.gohtml",
+		"tailwind.gohtml"))))
+
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS,
+		"faq.gohtml",
+		"tailwind.gohtml"))))
+
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS,
+		"signup.gohtml",
+		"tailwind.gohtml"))))
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	})
+
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
+
+}
