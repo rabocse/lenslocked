b30ab0f2f7129822a13bccf2b1ded8eb3fd04cdd
Alex <alexdescobars@gmail.com>
2023-04-19 11:36:59 +0100

    Parsed the signup form and printed it out

--- section-8/main.go
+++ section-8/main.go
@@ -35,6 +35,7 @@ func main() {
 		"tailwind.gohtml"))
 
 	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
