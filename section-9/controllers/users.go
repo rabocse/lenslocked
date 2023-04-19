3a2666f68cf5ef296b1f8874b5dadfd7a43f7961
Alex <alexdescobars@gmail.com>
2023-04-19 14:51:57 +0100

    Added a docker service for the postgres database

---
+++ section-9/controllers/users.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Users struct {
+	Templates struct {
+		New Template
+	}
+}
+
+func (u Users) New(w http.ResponseWriter, r *http.Request) {
+
+	var data struct {
+		Email string
+	}
+
+	data.Email = r.FormValue("email")
+	u.Templates.New.Execute(w, data)
+
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Email: ", r.FormValue("email"))       // From the signup.gohtml: <input name="email"
+	fmt.Fprint(w, "Password: ", r.FormValue("password")) // From the signup.gohtml: <input name="password"
+
+}
---
+++ section-9/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/rabocse/lenslocked/controllers"
+	"github.com/rabocse/lenslocked/templates"
+	"github.com/rabocse/lenslocked/views"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS,
+		"home.gohtml",
+		"tailwind.gohtml"))))
+
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS,
+		"contact.gohtml",
+		"tailwind.gohtml"))))
+
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS,
+		"faq.gohtml",
+		"tailwind.gohtml"))))
+
+	// the signup resource is a user action, therefore it might makes sense to move it
+	// to a users controller package, instead of using the static.go (staticHandler).
+	// This does not produce any visual difference
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS,
+		"signup.gohtml",
+		"tailwind.gohtml"))
+
+	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+	})
+
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
+
+}
